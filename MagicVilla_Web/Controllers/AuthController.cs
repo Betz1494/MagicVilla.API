752943c898ed7cb0708a17fbf7b62eb40075152d
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2023-01-04 21:28:18 -0600

    [ feat ] : Se agrega controller de autentificación y sus metodos correspondientes.

---
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO();
+            return View(loginRequestDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login( LoginRequestDTO loginRequestDTO)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterationRequestDTO registerDTO)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(registerDTO);
+            if(result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoDenied()
+        {
+            return View();
+        }
+    }
+}
