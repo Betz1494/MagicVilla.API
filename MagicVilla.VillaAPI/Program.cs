c68d461896ded7b01f20da0350cbd7d757eb8718
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2023-01-22 15:58:19 -0600

    [ feat ] : Se agrega versionamiento en API services.

--- MagicVilla.VillaAPI/Program.cs
+++ MagicVilla.VillaAPI/Program.cs
@@ -6,6 +6,7 @@ using MagicVilla.VillaAPI.Logging;
 using MagicVilla.VillaAPI.Repository;
 using MagicVilla.VillaAPI.Repository.IRepository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -22,6 +23,11 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
 
 var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
 
