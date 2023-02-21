c7ad67b7d6b1b8bdb286df5b3a48fe46754a1b63
Betzaith Aburto Guevara <betzaithaburto@gmail.com>
2023-02-21 15:17:19 -0600

    [ feat ] : Se mueve debido a que la version es general.

---
+++ MagicVilla.VillaAPI/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using MagicVilla.VillaAPI.Models;
+using MagicVilla.VillaAPI.Models.Dto;
+using MagicVilla.VillaAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MagicVilla.VillaAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/UsersAuth")]
+    [ApiController]
+    [ApiVersionNeutral]
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        protected APIResponse _response;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _response = new APIResponse();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDTO model)
+        {
+            var loginResponse = await _userRepository.Login(model);
+            if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.Errors.Add("Usuario o password incorrecto");
+                return BadRequest(_response);
+            }
+
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = loginResponse;
+
+            return Ok(_response);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterationRequestDTO model)
+        {
+            bool userUnique = _userRepository.IsUniqueUser(model.Usuario);
+            if (!userUnique)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.Errors.Add("Este usuario ya existe");
+                return BadRequest(_response);
+            }
+
+            var usuario = await _userRepository.Register(model);
+            if (usuario == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.Errors.Add("Ocurrio un error al registrar usuario");
+                return BadRequest(_response);
+            }
+
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+
+            return Ok(_response);
+        }
+    }
+}
