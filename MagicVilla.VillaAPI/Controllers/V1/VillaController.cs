0167fbbefe08ef6c12e1f69f1365d8eb4a0dfc29
Betzait <betzaithaburto@gmail.com>
2023-03-24 18:52:45 -0600

    Revert "[ style ] : Limpieza de codigo."
    This reverts commit ca4ffae1f7b05e4b2c8611c91e959826eacb3215.

--- MagicVilla.VillaAPI/Controllers/V1/VillaController.cs
+++ MagicVilla.VillaAPI/Controllers/V1/VillaController.cs
@@ -104,7 +104,7 @@ namespace MagicVilla.VillaAPI.Controllers.v1
                     //_response.IsSuccess = false;
                     //_response.Errors.Add($"La villa con id: {id} no fue encontrada.");
                     ModelState.AddModelError("Errors", $"La villa con id: {id} no fue encontrada.");
-                    return NotFound(ModelState); 
+                    return NotFound(ModelState); //_response
                 }
 
                 _response.Result = _mapper.Map<VillaDTO>(villa);
